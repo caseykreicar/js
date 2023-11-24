d926313e9e749796329a2c05defd46ccb611d3c6
Casey Latimere Kreicar <kreicarcl@alumni.vcu.edu>
2023-11-24 16:38:15 -0500

    js funtion with arguments

---
+++ functions.js
@@ -0,0 +1,8 @@
+//define function (argument, argument)
+function functionWithArguments(parameter1, parameter2) {
+    //print (argument1 + argument2)
+    console.log(parameter1 + parameter2)
+  }
+  //call function (argument1, argument2)
+  functionWithArguments(1, 2);
+  functionWithArguments(7, 9);
